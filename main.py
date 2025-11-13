5960efd8cd4b5f024dc680d0bf3edf32a40f96c7
gordonzev914-lang <gordonzev914@gmail.com>
2025-11-13 12:46:50 +0200

    v

--- main.py
+++ main.py
@@ -1,1 +1,46 @@
-ECHO is on.
+import uvicorn
+import json
+from fastapi import FastAPI, HTTPException
+from utlls import caesar
+from utlls import basec1
+from utlls import basec2
+
+
+app = FastAPI()
+
+
+
+@app("/test")
+def testing(test):
+    func1=basec1(test)
+    return {"maseg":func1}
+
+
+
+@app("/test")
+def testing(name):
+    func=basec2(name)
+    return {"maseg":func}
+
+
+
+
+
+
+@app("/caesar")
+def decrpting_caesar(text: str, offset: int):
+    the_encrypter=caesar(text,offset)
+    return the_encrypter 
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__": 
+    uvicorn.run(app, host="localhost", port=8000)
--- utlls.py
+++ utlls.py
@@ -1,1 +1,26 @@
-ECHO is on.
+
+def caesar(text,scep):
+    s=[]
+    for i in text:
+        num1=ord(i)
+        num2=num1+scep
+        if num2>=122:
+            num3=num2-25
+        else:
+            num3=num2    
+        s.append(chr(num3))
+    string1="".join(s)
+    return {"decrpted":string1.strip("'")}  
+
+def basec1(test):
+    return "hi from test"
+
+
+
+def basec2(name):
+    return f"hello {name}"
+
+
+
+        
+
